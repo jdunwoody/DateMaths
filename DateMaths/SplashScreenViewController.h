18cd3c69de180623903b22fa25fb19a2c6d240ed
James Dunwoody <jdunwoody@odecee.com.au>
2015-05-05 23:28:41 +1000

    Splash screen with timer

---
+++ DateMaths/SplashScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  SplashScreenViewController.h
+//  DateMaths
+//
+//  Created by James Dunwoody on 5/05/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashScreenViewController : UIViewController
+
+@end
---
+++ DateMaths/SplashScreenViewController.m
@@ -0,0 +1,35 @@
+//
+//  SplashScreenViewController.m
+//  DateMaths
+//
+//  Created by James Dunwoody on 5/05/2015.
+//
+//
+
+#import "SplashScreenViewController.h"
+
+@interface SplashScreenViewController ()
+
+@property (nonatomic, strong) NSTimer *timer;
+@end
+
+@implementation SplashScreenViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(timerFireMethod:) userInfo:nil repeats:NO];
+}
+
+- (void)timerFireMethod:(NSTimer *)timer
+{
+    [self performSegueWithIdentifier:@"dateMaths" sender:self];
+}
+
+- (void)timerFireMethod2
+{
+    [self performSegueWithIdentifier:@"dateMaths" sender:self];
+}
+
+@end
